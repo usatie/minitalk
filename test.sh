119fefd20ab9dae86a4ecd1faf6e9d1e7a12917c
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-05-27 15:22:15 +0900

    :recycle: Refactor test script

--- test.sh
+++ test.sh
@@ -16,14 +16,25 @@ do
 done
 
 # Before starting tests, kill all server processes
-printf "Before starting tests, kill all server processes...\n\n"
 ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
 
-# Launch server
-echo "Launch server"
-./server | tail +2 | diff <(printf $EXPECTED) - && printf "\e[1;32mdiff OK:D\n\e[m" || printf "\e[0;31mdiff error\n\e[m" &
+# 1. Testing a message from single process
+printf "[1. Testing a message from single process]\n"
+for msg in "${MSGS[@]}"
+do
+	./server | tail +2 | diff <(printf $msg) - && printf "\e[1;32mdiff OK:D\n\e[m" || printf "\e[0;31mdiff error\n\e[m" &
+	SERVER_PID=$(ps | grep "./server" | grep -v grep | head -1 | awk '{printf $1}')
+	printf "./client $SERVER_PID $msg\n"
+	./client $SERVER_PID $msg
+	ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
+done
 
-# Launch clients continuously
+sleep 1
+printf "\n\n"
+
+# 2. Testing messages from multiple processes in a row
+printf "[2. Testing messages from multiple processes in a row]\n"
+./server | tail +2 | diff <(printf $EXPECTED) - && printf "\e[1;32mdiff OK:D\n\e[m" || printf "\e[0;31mdiff error\n\e[m" &
 SERVER_PID=$(ps | grep "./server" | grep -v grep | head -1 | awk '{printf $1}')
 for msg in "${MSGS[@]}"
 do
@@ -32,5 +43,4 @@ do
 done
 
 # After the tests, kill all server processes
-printf "After the tests, kill all server processes...\n\n"
 ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
