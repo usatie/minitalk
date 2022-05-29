3bf6cab9cccb0b211f8b61ad6d37b47fc8acd298
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-05-29 21:51:41 +0900

    :recycle: Refactor and update test script to count success/error tests

--- test.sh
+++ test.sh
@@ -1,46 +1,59 @@
 #!/bin/bash
-set -e
+# Test Configuration
+NUM_REPEAT=25
+MSG_LEN=1000
+printf "NUM_REAPEAT=$NUM_REPEAT, MSG_LEN=$MSG_LEN\n"
 
 # Test Cases
-MSG_LEN=1000
-printf "MSG_LEN=$MSG_LEN\n"
+messages+=("hello")
+messages+=("world")
+messages+=($(yes "0123456789" | tr -d '\n' | head -c $MSG_LEN))
 
-MSG1="hello"
-MSG2="world"
-MSG3=$(yes "0123456789" | tr -d '\n' | head -c $MSG_LEN)
-
-MSGS=($MSG1 $MSG2 $MSG3)
-for msg in "${MSGS[@]}"
+for msg in "${messages[@]}"
 do
-	    EXPECTED+=$msg
+	combined_message+=$msg
 done
+
+success=0
+total=0
 
 # Before starting tests, kill all server processes
 ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
 
-# 1. Testing a message from single process
-printf "[1. Testing a message from single process]\n"
-for msg in "${MSGS[@]}"
+for (( i = 0; i < $NUM_REPEAT; i++ ))
 do
-	./server | tail +2 | diff <(printf $msg) - && printf "\e[1;32mdiff OK:D\n\e[m" || printf "\e[0;31mdiff error\n\e[m" &
-	SERVER_PID=$(ps | grep "./server" | grep -v grep | head -1 | awk '{printf $1}')
-	printf "./client $SERVER_PID $msg\n"
-	./client $SERVER_PID $msg
+	# 1. Testing a message from single process
+	printf "[1. Testing a message from single process]\n"
+	for msg in "${messages[@]}"
+	do
+		((total+=1))
+		./server | tail +2 | diff <(printf $msg) - &
+		pid=$(ps | grep "./server" | grep -v grep | head -1 | awk '{printf $1}')
+		printf "./client $pid $msg\n"
+		./client $pid $msg
+		ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
+		wait $! && printf "\e[1;32mdiff OK:D\n\e[m" && ((success+=1)) || printf "\e[0;31mdiff error\n\e[m"
+	done
+
+	# 2. Testing messages from multiple processes in a row
+	printf "[2. Testing messages from multiple processes in a row]\n"
+	((total+=1))
+	./server | tail +2 | diff <(printf $combined_message) - &
+	pid=$(ps | grep "./server" | grep -v grep | head -1 | awk '{printf $1}')
+	for msg in "${messages[@]}"
+	do
+		printf "./client $pid $msg\n"
+		./client $pid $msg
+	done
+
+	# After the tests, kill all server processes
 	ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
+	wait $! && printf "\e[1;32mdiff OK:D\n\e[m" && ((success+=1)) || printf "\e[0;31mdiff error\n\e[m"
 done
 
-sleep 0.1
-printf "\n\n"
-
-# 2. Testing messages from multiple processes in a row
-printf "[2. Testing messages from multiple processes in a row]\n"
-./server | tail +2 | diff <(printf $EXPECTED) - && printf "\e[1;32mdiff OK:D\n\e[m" || printf "\e[0;31mdiff error\n\e[m" &
-SERVER_PID=$(ps | grep "./server" | grep -v grep | head -1 | awk '{printf $1}')
-for msg in "${MSGS[@]}"
-do
-	printf "./client $SERVER_PID $msg\n"
-	./client $SERVER_PID $msg
-done
-
-# After the tests, kill all server processes
-ps | grep "./server" | grep -v grep | awk '{print $1}' | xargs -n1 kill
+if [ $success == $total ]
+then
+	printf "\n\e[1;32mAll $total tests passed!\n\e[m";
+else
+	printf "\n\e[1;31m$((total - success)) tests failed. $success tests passed.\n\e[m";
+fi
